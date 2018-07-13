b81a75944e3858c431b231396f265a2db2621b81
Haley Mendoza <mendozahaley@gmail.com>
2018-07-12 21:16:44 -0700

    quiz model methods with basic code examples

---
+++ src/models/quiz.js
@@ -0,0 +1,55 @@
+// const import fs from 'fs';
+// var obj = JSON.parse(fs.readFileSync('file', 'utf8'));
+// import three other files containing other objects until we are done, to avoid merge conflicts
+
+import quizRandom from './quizRandom.js';
+
+export default class Quiz {
+  findOne(day){
+    fetch();
+    process();
+    quizRandom();
+    // fetch, process, magic
+    // method to fetch that returns array of objects
+    // method to process that data that returns an array
+    // method to sort, randomize, return
+  }
+
+  fetch(){
+    let obj = [
+      {
+        "question": "What year is it?",
+        "answers": [
+          2019,
+          2018,
+          2017,
+          2016
+        ]
+      },
+      {
+        "question": "Who is Billy Jean?",
+        "answers": [
+          "Not my lover",
+          "Just a girl",
+          "A beauty queen",
+          "All of the above"
+        ]
+      },
+    ],
+    // returns an array of arrays containing objects 01: [[{questions/answers}, [{questions/answers}], etc..]
+  }
+
+  process(){
+    // do some function that flattens the JSON object into an array
+    let allQuizzes = ['apple', 'banana', 'mango', 'strawberry', 'kiwi', 'peach', 'red vines', 'blackberry', 'cherry', 'lime'];
+  }
+  fetchForReal(){
+    // has same reutrn and signature as above function until fetch is finished 
+  }
+
+  quizRandom(){
+    // has method that A & J are working on 
+  }
+
+
+}
