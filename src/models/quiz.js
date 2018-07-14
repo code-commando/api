6e695b8d21257afe029060c9cb91c93be63285de
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-07-14 16:21:59 -0700

    github shut down

--- src/models/quiz.js
+++ src/models/quiz.js
@@ -6,21 +6,18 @@
 // import quizRandom from './quizRandom.js';
 import superagent from 'superagent';
 
-const listContent = require('list-github-dir-content');
-const myToken = process.env.GITHUB_TOKEN;
-
 // export default class Quiz{
-  // static findOne(){
-  //   console.log('here were some data');
-  // }
+// static findOne(){
+//   console.log('here were some data');
+// }
 //   static find(){
 //     return Promise.resolve('a string');
 //   }
 // }
 
 export default class Quiz {
-  static findOne(day){
-    return Quiz.fetch();
+  static findOne(day) {
+    return Quiz.fetch(day);
     // process();
     // quizRandom();
     // fetch, process, magic
@@ -29,76 +26,65 @@ export default class Quiz {
     // method to sort, randomize, return
   }
 
-  static fetch(){
+  static fetch(day) {
     // 1. Get a list of folders from repo. 2. Get the file contents from each quiz.json for each class before today 
-    return superagent.get('https://github.com/Bolstad09/sample-class')
-      .then(response => {
-        // const filesArray = listContent.viaContentsApi('Microsoft/vscode', 'src', myToken);
-        // return filesArray;
-        return ['redvines'];
-      })
-      .catch(err => console.log(err));
-    // returns an array of arrays containing objects 01: [[{questions/answers}, [{questions/answers}], etc..]
+    return superagent.get('https://api.github.com/repos/code-commando/sample-class/contents/')
+      .then(arr => {
+        let dayArr = JSON.parse(arr.text);
+        let urlArr = dayArr.reduce((list, classDay) => {
+          let classNum = parseInt(classDay.name.split('-')[0].replace(/^0/, ''));
+          if (classNum < day) list.push(classDay.url);
+          return list;
+        }, []);
+        let requests = [];
+        for (let i = (urlArr.length - 1); i >= 0; i--) {
+          console.log(urlArr[i] + '/quiz.json');
+          requests.push(superagent.get(urlArr[i] + '/quiz.json'));
+        }
+        //return this
+        Promise.all(requests).then(data => {
+          data.forEach(result => {
+            // ^^ for loop not forEach
+            //create an array of ALL quizzes
+            // we may want result.body or result.text maybe JSON parse?
+            
+            console.log(result);
+          });
+        });
+      });
   }
 }
-
-  process(){
-    let quizArr = [
-      {
-        'question': 'What year is it?',
-        'answers': [
-          2019,
-          2018,
-          2017,
-          2016
-        ]
-      },
-      {
-        'question': 'Who is Billy Jean?',
-        'answers': [
-          'Not my lover',
-          'Just a girl',
-          'A beauty queen',
-          'All of the above'
-        ]
-      },
-      {
-        "question": "What is an advantage of doing TDD?",
-        "correctAnswer": "Faster development, solid code, well tested edge cases"
-      }
-    ]
+// returns an array of arrays containing objects 01: [[{questions/answers}, [{questions/answers}], etc..]
 
 
-});
-    // do some function that flattens the JSON object into an array
-    let allQuizzes = ['apple', 'banana', 'mango', 'strawberry', 'kiwi', 'peach', 'red vines', 'blackberry', 'cherry', 'lime'];
-  }
-  /*
-  fetchForReal(){
-    // has same reutrn and signature as above function until fetch is finished 
-  }
+//   process(){
 
-  quizRandom(){
-    // has method that A & J are working on 
+// }
 
 
-  }
+// fetchForReal(){
+//   // has same reutrn and signature as above function until fetch is finished 
+// }
+
+// quizRandom(){
+//   // has method that A & J are working on 
 
 
-  // /api/v1/quiz/12 
-  // Expect 5 random questions from days 1 through ##-1. 
-  // Output expected:
-  // {
-  //   count: 5, 
-  //   results: [
-  //     {
-  //       question: 'Some question?', 
-  //       answers: [1,2,3,4],
-  //       correctAnswer: '4'
-  //     }, 
-  //     ...
-  //   ]
-  // }
-  // Todays goal: fetch  a /quiz.json from  each day's folder in guide repo
-}
-*/
+// }
+
+
+// /api/v1/quiz/12 
+// Expect 5 random questions from days 1 through ##-1. 
+// Output expected:
+// {
+//   count: 5, 
+//   results: [
+//     {
+//       question: 'Some question?', 
+//       answers: [1,2,3,4],
+//       correctAnswer: '4'
+//     }, 
+//     ...
+//   ]
+// }
+// Todays goal: fetch  a /quiz.json from  each day's folder in guide repo
