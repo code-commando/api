d2329693d2247c3aeadc964f9122b05a447d56b8
Ariel Altaras <ariel.altaras@gmail.com>
2018-07-14 09:29:48 -0700

    updated random function to ensure no repeated questions

--- src/models/quizRandom.js
+++ src/models/quizRandom.js
@@ -3,13 +3,24 @@
 let allQuizzes = ['apple', 'banana', 'mango', 'strawberry', 'kiwi', 'peach', 'red vines', 'blackberry', 'cherry', 'lime'];
 
 let randomIndex = 0;
-let questions = [];
 
 function randomQuiz () {
+  let questions = [];
+  let selected = [];
   let maxIndex = allQuizzes.length;
   for (let i = 0; i < 5; i++) {
     randomIndex = Math.floor((Math.random() * maxIndex));
-    questions.push(allQuizzes[randomIndex]);
-    console.log(questions);    
+    if (selected.includes(randomIndex)) {
+      i--;
+    }
+    else {
+      selected.push(randomIndex);
+    }
   }
+  for (let i = 0; i < selected.length; i++) {
+    questions.push(allQuizzes[selected[i]]);
+  }
+  console.log(questions);    
 }
+
+randomQuiz();
