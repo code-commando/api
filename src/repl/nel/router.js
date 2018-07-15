b66c4628748942ea16e22e43513c4c95304b74e4
Mrebb <madhu.rebbana@gmail.com>
2018-07-14 18:24:46 -0700

    post&put to github from frontend to all the way working

--- src/repl/nel/router.js
+++ src/repl/nel/router.js
@@ -1,25 +1,46 @@
 import express from 'express';
 const router = express.Router();
 import nel from 'nel';
-
-// router.get('api/v1/editor',(req,res)=>{
-// //   monaco.editor.create(document.getElementById('container'), {
-// //     value: 'function hello() {\n\talert(\'Hello world!\');\n}',
-// //     language: 'javascript',
-// //   });
-//   console.log('We are here');
-//   res.send('hello');
-// });
+import superagent from 'superagent';
+import fs from 'fs';
 
 let solution = {};
 let onStdoutArray = [];
 
 router.post('/api/v1/code', (req, res) => {
-  let session = new nel.Session();
-  console.log('the code -->', req.body.code);
   let code = req.body.code;
   let fileName = req.body.fileName;
   let day = req.body.day;
+  //const upload = multer({dest: `${__dirname}/../tmp`});
+  if ( ! fileName ) { 
+    return('No fileName Specified'); 
+  }
+  let file = `${__dirname}/../tmp/${fileName}.js`;
+  console.log(file);
+  let text = code;
+  fs.writeFile( file, text, (err) => {
+    if(err) { return(err); }
+    console.log('file Saved to temp!');
+  });
+  let encodedBuffer = new Buffer(code);
+  let base64Encoded = encodedBuffer.toString('base64');
+  let githubObject = {
+    'message': 'mid-term',
+    'committer': {
+      'name': 'MR',
+      'email': 'madhu.rebbana@gmail.com',
+    },
+    'content': base64Encoded,
+    'sha':'89194f6b0b79584a48e0e3bcd5ee01dafabd128c',
+  };
+  superagent.put(`https://api.github.com/repos/code-commando/sample-class/contents/01-node-ecosystem/${fileName}.js`)
+    .set('Authorization', `Basic bXJlYmI6YW1tdTIzMDg=`)
+    .send(githubObject)
+    .then(response=>console.log(response))
+    .catch(err=>console.log('error',err));
+  let session = new nel.Session();
+  console.log('the code -->', req.body.code);
+  
   solution.input = code;
   session.execute(code, {
     onSuccess: (output) => {
