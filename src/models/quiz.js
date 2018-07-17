f546a875cf13ffafcf280f9657ce85d79e5d1fd8
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-07-16 21:19:23 -0700

    process almost done

--- src/models/quiz.js
+++ src/models/quiz.js
@@ -17,14 +17,19 @@ import superagent from 'superagent';
 
 export default class Quiz {
   static findOne(day) {
-    return Quiz.fetch(day);
-    // process();
+    return Quiz.fetch(day).then(quizzes => {
+      console.log({quizzes});
+      Quiz.process(quizzes);
+    });
+  }
+  
+ 
     // quizRandom();
     // fetch, process, magic
     // method to fetch that returns array of objects
     // method to process that data that returns an array
     // method to sort, randomize, return
-  }
+  
 
   static fetch(day) {
     // 1. Get a list of folders from repo. 2. Get the file contents from each quiz.json for each class before today 
@@ -32,73 +37,41 @@ export default class Quiz {
       .then(data => {
         let dayArr = JSON.parse(data.text);
         let urlArr = dayArr.reduce((list, classDay) => {
-          let classNum = parseInt(classDay.name.split('-')[0].replace(/^0/, ''));
+          let classNum = parseInt(classDay.name.split('-')[0]);
           if (classNum < day) list.push(classDay.html_url);
           return list;
         }, []);
         let requests = [];
         for (let i = (urlArr.length - 1); i >= 0; i--) {
-          let newUrl = urlArr[i].replace('github', 'raw.githubusercontent').replace('/blob', '') + '/quiz.json';
-          console.log(newUrl);
+          let newUrl = urlArr[i].replace('github', 'raw.githubusercontent').replace('/blob', '').replace('/tree', '') + '/quiz.json';
+          // console.log(newUrl);
           requests.push(superagent.get(newUrl));
         }
-        // https://raw.githubusercontent.com/code-commando/sample-class/master/01-node-ecosystem/quiz.json
 
-        // https://github.com/code-commando/sample-class/blob/master/01-node-ecosystem/quiz.json
-        //return this
-        Promise.all(requests).then(responses => {
-          let quizzes = responses.map(response => response.body);
-          console.log(quizzes);
-          return quizzes;
-          // for (let i = 0; i < quizzes.length; i++) {
-            
-            
-          // }
-          
+        return Promise.all(requests).then(responses => {
+          let quizzes = responses.map(response => JSON.parse(response.text));
+          // console.log(quizzes);
+          return quizzes;          
         })
           .catch(err => {
             console.log(err);
           });
-          
-          // ^^ for loop not forEach
-          //create an array of ALL quizzes
-          // we may want result.body or result.text maybe JSON parse?
-            
-          
-        
       });
   }
+
+  static process(quizArr) {
+    let newQuizArr = [];
+    console.log(quizArr);
+    quizArr.forEach((singleQuiz) => {
+      singleQuiz.forEach((question) => {
+        newQuizArr.push(question);
+      });
+      console.log(newQuizArr);
+    });
+  }
 }
-
-
-
-// returns an array of arrays containing objects 01: [[{questions/answers}, [{questions/answers}], etc..]
-
-//   process(){
-
-// }
-
-// fetchForReal(){
-//   // has same reutrn and signature as above function until fetch is finished 
-// }
 
 // quizRandom(){
 //   // has method that A & J are working on 
 
 // }
-
-// /api/v1/quiz/12 
-// Expect 5 random questions from days 1 through ##-1. 
-// Output expected:
-// {
-//   count: 5, 
-//   results: [
-//     {
-//       question: 'Some question?', 
-//       answers: [1,2,3,4],
-//       correctAnswer: '4'
-//     }, 
-//     ...
-//   ]
-// }
-// Todays goal: fetch  a /quiz.json from  each day's folder in guide repo
