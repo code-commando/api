81866714db00fc85ef47ad156ae558194a380a42
Phillip Kim <phildkim@gmail.com>
2018-07-19 21:17:17 -0700

    added classes routes in swagger

--- src/models/classes.js
+++ src/models/classes.js
@@ -1,13 +1,12 @@
 'use strict';
 
 import mongoose from 'mongoose';
-// import userSchema from './user.js';
 
 const classesSchema = mongoose.Schema({
   classCode: {type: String, required: true},
-  canvasClassId: {type: String, required: true, unique: true},
-  githubRepo: {type: String, required: true,  unique: true},
-//  authorizedUSers: {type: [userSchema]}, 
+  canvasClassId: {type: String},
+  githubRepo: {type: String, required: true},
+  apiLink: {type: String},
 });
 
 export default mongoose.model('classes', classesSchema);
